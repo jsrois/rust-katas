45078ea34859c8946b4b161ba534ba8ba771611a
Javi <javi@jsrois.net>
2025-11-30 17:53:03 +0100

    kata

---
+++ password_validator/src/lib.rs
@@ -0,0 +1,14 @@
+pub trait Password {
+    fn validate(&self) -> bool;
+}
+
+impl Password for String {
+    fn validate(&self) -> bool {
+        self.len() >= 12 &&
+            self.chars().any(|c| c.is_lowercase()) &&
+            self.chars().any(|c: char| c.is_uppercase()) &&
+            self.chars().any(|c: char| c.is_numeric()) &&
+            self.chars().any(|c: char| ['!','@','#','$','%','^','&','*'].contains(&c))
+    }
+}
+
